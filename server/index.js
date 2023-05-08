dd810e28711d5a963a98a196114fed9cfa0eb578
prashant_rathour <prathour884@gmail.com>
2023-05-08 14:11:55 +0530

    socket.io

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { Server, socket } = require("socket.io");
+const io = new Server({
+  cors: true,
+});
+const app = express();
+app.use(bodyParser.json());
+const emailToSocketMapping = new Map();
+const socketidtoEmailMapping = new Map();
+io.on("connection", (socket) => {
+  console.log("new connection");
+  socket.on("join-room", (data) => {
+    const { roomId, emailId } = data;
+    console.log("user", emailId, roomId);
+    emailToSocketMapping.set(emailId, socket.id);
+    socketidtoEmailMapping.set(socket.id, emailId);
+    socket.join(roomId);
+    socket.emit("joined-room", { roomId });
+    socket.broadcast.to(roomId).emit("user-joined", { emailId });
+  });
+  socket.on("call-user", data => {
+    const { emailId, offer } = data;
+    const fromEmail = socketidtoEmailMapping.get(socket.id);
+    const socketId = emailToSocketMapping.get(emailId);
+    socket.to(socketId).emit("incomming-call", { from: fromEmail, offer });
+  });
+  socket.on('call-accepted',data=>{
+    const {emailId,ans}=data;
+    const socketId = emailToSocketMapping.get(emailId);
+    socket.to(socketId).emit("call-accepted", { ans})
+  })
+});
+app.listen(8000, () => console.log("Http server running..."));
+io.listen(8001);
